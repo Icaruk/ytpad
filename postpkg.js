cea401277cb77c53b8a5c3708fd476cb0749af34
Icaruk <adri-rk@hotmail.com>
2021-08-06 00:36:28 +0200

    automate postbuild zip

--- postpkg.js
+++ postpkg.js
@@ -1,8 +1,16 @@
 
 const fs = require("fs");
 const {version} = require("./package.json");
+var AdmZip = require('adm-zip');
 
 
-const nombre = `ytpad_${version}.exe`;
+const nombre = `ytpad_${version}`;
+const outPath = `./build/${nombre}`;
 
-fs.renameSync("./build/pkg.exe", `./build/${nombre}`);
+fs.renameSync("./build/pkg.exe", `${outPath}.exe`);
+
+
+const zip = new AdmZip();
+zip.addLocalFile(`${outPath}.exe`);
+zip.writeZip(`${outPath}.zip`);
+
